05641cc0714b0660a9ea8a0c3f616dc7b10782cf
samimoke <107768575+samimoke@users.noreply.github.com>
2024-02-26 23:33:54 +0300

    pages are added

--- graphics/vidgraph/urls.py
+++ graphics/vidgraph/urls.py
@@ -1,5 +1,16 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 urlpatterns = [
     path('',views.home, name='home'),
+    path('about',views.about, name='about'),
+    path('contactus',views.contactus, name='contactus'),
+    path('portfolio',views.portfolio, name='portfolio'),
+    path('blog',views.blog, name='blog'),
+    path('service', views.service, name='service'),
+    path('blog-detail', views.blogDetail, name='blog-detail')
+
+    
 ]
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- graphics/vidgraph/views.py
+++ graphics/vidgraph/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('good')
+    return render(request,'index.html')
+def about(request):
+    return render(request,'about.html')
+def contactus(request):
+    return render(request,'contact.html' )
+def portfolio(request):
+    return render(request, 'portfolio.html')
+def blog(request):
+    return render(request,'blog.html')
+def service(request):
+    return render(request, 'services.html')
+def blogDetail(request):
+    return render(request,'blog-details.html')
